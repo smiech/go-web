20c16db358172b46309bc9596b47a0bb7c9829b9
Adam Smiechura <adam@72media.nl>
2017-09-22 14:41:15 +0200

    add JWT auth

--- go-web.go
+++ go-web.go
@@ -7,7 +7,11 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/auth0/go-jwt-middleware"
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
 )
@@ -86,14 +90,74 @@ func main() {
 	configureLogger()
 
 	r := mux.NewRouter()
-
 	de := Device{Id: "aa", Name: "bb"}
 	de.save()
-	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./client/dist/")))
-	r.Handle("/api/tokenauth", NotImplemented).Methods("POST")
-	http.ListenAndServe(":8080", r)
+
+	r.Handle("/get-token", GetTokenHandler).Methods("GET")
+	r.Handle("/status", jwtMiddleware.Handler(ProductsHandler)).Methods("GET")
+	r.PathPrefix("/").Handler(jwtMiddleware.Handler(http.FileServer(http.Dir("./client/dist/"))))
+	http.ListenAndServe(":8080", handlers.LoggingHandler(os.Stdout, r))
 }
 
-var NotImplemented = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Not Implemented"))
+/* Set up a global string for our secret */
+var mySigningKey = []byte("secret")
+
+/* Handlers */
+var GetTokenHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	/* Create the token */
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	/* Create a map to store our claims*/
+	claims := token.Claims.(jwt.MapClaims)
+
+	/* Set token claims */
+	claims["iss"] = "golang"
+	claims["name"] = "Ado Kukic"
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+
+	/* Sign the token with our secret */
+	tokenString, _ := token.SignedString(mySigningKey)
+
+	/* Finally, write the token to the browser window */
+	w.Write([]byte(tokenString))
+})
+
+var jwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
+	ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+		return mySigningKey, nil
+	},
+	SigningMethod: jwt.SigningMethodHS256,
+})
+
+type Product struct {
+	Id          int
+	Name        string
+	Slug        string
+	Description string
+}
+
+/* We will create our catalog of VR experiences and store them in a slice. */
+var products = []Product{
+	Product{Id: 1, Name: "Hover Shooters", Slug: "hover-shooters", Description: "Shoot your way to the top on 14 different hoverboards"},
+	Product{Id: 2, Name: "Ocean Explorer", Slug: "ocean-explorer", Description: "Explore the depths of the sea in this one of a kind underwater experience"},
+	Product{Id: 3, Name: "Dinosaur Park", Slug: "dinosaur-park", Description: "Go back 65 million years in the past and ride a T-Rex"},
+	Product{Id: 4, Name: "Cars VR", Slug: "cars-vr", Description: "Get behind the wheel of the fastest cars in the world."},
+	Product{Id: 5, Name: "Robin Hood", Slug: "robin-hood", Description: "Pick up the bow and arrow and master the art of archery"},
+	Product{Id: 6, Name: "Real World VR", Slug: "real-world-vr", Description: "Explore the seven wonders of the world in VR"},
+}
+
+/* The status handler will be invoked when the user calls the /status route
+It will simply return a string with the message "API is up and running" */
+var StatusHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("API is up and running"))
+})
+
+/* The products handler will be called when the user makes a GET request to the /products endpoint.
+This handler will return a list of products available for users to review */
+var ProductsHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// Here we are converting the slice of products to json
+	payload, _ := json.Marshal(products)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(payload))
 })
